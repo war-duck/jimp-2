4b31b4185ddce91825ff76a1528b5f85a1ab5423
Mikolaj <mikipatkowski@gmail.com>
2023-03-17 18:45:29 +0100

    edycja drzewa, dodanie niepełnej funkcji makeTree

--- src/drzewo binarne/tree.c
+++ src/drzewo binarne/tree.c
@@ -1,14 +1,16 @@
 #include "tree.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 // tworzy węzeł
-treeNode *addNode(char c, int num){
+treeNode *addNode(unsigned char c, int num){
     treeNode *node = malloc(sizeof *node);
     if(node != NULL){
         node->c = c;
         node->count = num;
         node->left = NULL;
         node->right = NULL;
+        node->is_leaf = 1;
     }
     return node;
 }
@@ -19,8 +21,8 @@ treeNode *joinNodes(treeNode *first, treeNode *second){
     if(newNode == NULL)
         return NULL;
     
-    newNode->c = -1;    // oznaczenie, że nowy węzeł nie zawiera żadnego znaku
-                        // znaki zawierają jedynie liście drzewa
+    newNode->c = 0;     
+    newNode->is_leaf = 0;   // Oznaczenie, że nowy węzeł nie jest liściem
     newNode->count = first->count + second->count;
     newNode->left = first;
     newNode->right = second;
@@ -42,7 +44,10 @@ static void printTreeRec(treeNode *node, int level){
         return;
     }
     printTabs(level);
-    printf("char: %d\n", node->c);
+    if(node->is_leaf)
+        printf("znak: [%c]\n", node->c);
+    else   
+        printf("<wezel>\n");
     printTabs(level);
     printf("count: %d\n", node->count);
     printTabs(level);
@@ -51,6 +56,39 @@ static void printTreeRec(treeNode *node, int level){
     printTabs(level);
     printf("right:\n");
     printTreeRec(node->right, level+1);
+}
+
+// Na podstawie priority queue tworzy pełne drzewo binarne
+treeNode *makeTree(queue *q){
+    queNode *current = q->head;
+    queNode *previous = NULL;
+    queNode *highestPrior = current;
+
+    queNode *lowestPrior1 = NULL;
+    queNode *lowestPrior2 = NULL;
+
+    while(current->next != NULL){  // Dopóki wielkość kolejki nie jest równa 1
+
+        while(current != NULL){ // Dopóki nie dojdzie do końca kolejki
+            if(current->prior < highestPrior->prior){
+                highestPrior = current;
+                lowestPrior2 = lowestPrior1;
+                lowestPrior1 = current;
+            }
+            else if (lowestPrior2 == NULL || current->prior < lowestPrior2->prior) {
+            lowestPrior2 = current;
+            }
+
+            current = current->next;
+        }
+
+        // połącz dwa drzewa (first i second) w nowe drzewo
+        // usuń dwa elementy (drzewa) z kolejki
+        // wstaw nowy element (drzewo) do kolejki
+
+        // wróć na początek kolejki
+        current = q->head;
+    }
 }
 
 // Inicjuje rekursywną funkcję wyświetlającą zawartość drzewa
--- src/drzewo binarne/tree.h
+++ src/drzewo binarne/tree.h
@@ -3,15 +3,19 @@
 #ifndef TREE_H
 #define TREE_H
 
+#include "..\priotiry queue\queue.h"
+
 typedef struct treeNode{
-    char c;
+    unsigned char c;
+    int is_leaf;
     int count;
     struct treeNode *left;
     struct treeNode *right;
 } treeNode;
 
-treeNode *addNode(char, int);
+treeNode *addNode(unsigned char, int);
 treeNode *joinNodes(treeNode*, treeNode*);
+treeNode *makeTree(queNode*);
 void printTree(treeNode*);
 
 #endif
