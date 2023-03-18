a89fb3db5fa23905969eb74b05ec664c601d0b23
Mikolaj <mikipatkowski@gmail.com>
2023-03-18 12:48:19 +0100

    Dodano liczenie wystepowania konkretnych bajtow

--- src/wczytywanie bajtów/wczytywanie.c
+++ src/wczytywanie bajtów/wczytywanie.c
@@ -8,7 +8,8 @@ int main(int argc, char** argv){
 
     int i;
     unsigned char *c;
-    long length;    // Długość pliku w bajtach
+    unsigned int *num;  // Liczba występowania poszczególnych bajtów
+    long length;        // Długość pliku w bajtach
 
     // Otwieranie pliku wejściowego
     FILE *in = fopen(argv[1], "rb");
@@ -24,9 +25,22 @@ int main(int argc, char** argv){
     size_t liczba_odczyt = fread(c, sizeof(*c), length, in);   // Wczytaj treść pliku
 
     printf("Liczba odczytanych bajtów: %d\n", liczba_odczyt);
-    printf("Wczytana tresc (tekstowa): %s", c);
+    printf("Wczytana tresc (tekstowa): %s\n", c);
 
+    num = calloc(256, sizeof *num);
+
+    // Policz występowanie poszczególnych bajtów
+    for(i = 0; i < length; i++){
+        num[c[i]]++;
+    }
+
+    printf("Czestotliwosc wystepowania konkretnych bajtow:\n");
+    for(i = 0; i < 256; i++)
+        printf("%x - %d\n", i, i, num[i]);
+    
     free(c);
+    free(num);
+    fclose(in);
 
     return 0;
 }
