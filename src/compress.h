0ae80bea97beff491bfc5a91fbd3cc7f23223970
war-duck <marcin.l.wardak@gmail.com>
2023-03-20 18:13:01 +0100

    modul kompresora - v1 dzialajaca

--- src/compress.h
+++ src/compress.h
@@ -9,10 +9,11 @@ typedef struct
 
 typedef struct
 {
-    char* data;
-    long len;
-    long max_size;
-    short byte_pos;
+    unsigned char* data; // surowe dane
+    long len;            // długość w bajtach zakodowanych danych (len(data))
+    long max_size;       // rozmiar zaalokowany
+    short byte_pos; // jeżeli == 0, to cały ostatni bajt (data[len-1]) jest zajęty.
+                    // jeżeli != 0, to część bajtu data[len-1] jest zajęta, należy przejść do kolejnego bitu
 } data_struct; // przechowuje zakodowaną treść pliku wejściowego
 
 #endif
