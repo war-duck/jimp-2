4436a24a6d4dac316fa62c28b2aa1aa9ae46672c
Mikolaj <mikipatkowski@gmail.com>
2023-03-26 18:28:59 +0200

    Odczytywanie treści pliku

--- src/decompressor/decompressor.c
+++ src/decompressor/decompressor.c
@@ -17,25 +17,44 @@ void print_binary(unsigned char c, int length) {
     }
     printf("\n");
 }
+
 int main(int argc, char** argv){
     FILE *in = fopen(argv[1], "rb");
+    long tracer = 0;    // znacznik, który bajt jest aktualnie czytany
+
+    fseek(in, 0, SEEK_END);     
+    long file_length = ftell(in);         
+    printf("Ilosc bajtow: %d\n", file_length);   
+    fseek(in, 0, SEEK_SET);
+
     int codes_num = fgetc(in);
+    tracer++;
     printf("Liczba znakow: %d\n", codes_num);
 
     char* symbols = malloc(codes_num * sizeof *symbols);
+    int* code_lengths = malloc(codes_num * sizeof code_lengths);
     unsigned char* codes = malloc(codes_num * sizeof *codes);
 
     printf("Slownik:\n");
     for(int i = 0; i < codes_num; i++){
         symbols[i] = fgetc(in);
-        int code_length = fgetc(in);
+        code_lengths[i] = fgetc(in);
         codes[i] = fgetc(in);
+        tracer += 3;
         printf("%c - ", symbols[i]);
-        print_binary(codes[i], code_length);
+        print_binary(codes[i], code_lengths[i]);
+    }
+
+    // Odczytaj treść pliku
+    while(tracer != file_length){
+        char byte = fgetc(in);
+        tracer++;
+        // Tutaj będzie kod odpowiedzialny za odkodowanie treści pliku
     }
 
 
     free(symbols);
+    free(code_lengths);
     free(codes);
     return 0;
 }
